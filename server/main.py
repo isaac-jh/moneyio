4186d640729699d64d49fe933c225a91aa7f8d6f
isaac <isaac@tpirates.com>
2025-04-04 11:11:25 +0900

    feat: main and settings

---
+++ server/main.py
@@ -0,0 +1,32 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# from routers import api_gateway
+# from utils.exceptions import register_exception
+from utils.settings import get_settings
+
+# from uvicorn.config import LOGGING_CONFIG
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.include_router(api_gateway.router)
+# register_exception(app)
+
+if __name__ == "__main__":
+
+    # LOG FORMATTING
+    # LOGGING_CONFIG["formatters"]["default"]["fmt"] = get_settings().logfmt
+    # LOGGING_CONFIG["formatters"]["access"]["fmt"] = get_settings().logfmt_access
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=get_settings().uvicorn_reload,
+    )
---
+++ server/utils/settings.py
@@ -0,0 +1,30 @@
+import os
+from functools import cache
+
+from pydantic import AnyHttpUrl, BaseSettings
+
+
+class PostgresDBSettings(BaseSettings):
+    db_url: str
+    db_port: int
+    db_username: str
+    db_password: str
+    db_database: str
+
+
+class Settings(PostgresDBSettings):
+    appname: str = "moneyio"
+    loglevel: str = "INFO"
+    logfmt: str = "%(asctime)s %(levelname)-8s %(name)-12s: %(message)s"
+    logfmt_access: str = "%(asctime)s %(levelname)-8s %(name)-12s: %(message)s"
+    uvicorn_reload: bool = False
+
+
+# settings 값을 여러번 반환해주기 때문에 cache 사용
+@cache
+def get_settings():
+    envfile = os.environ.get("ENVFILE")
+    if envfile is not None:
+        return Settings(_env_file=envfile)
+
+    return Settings()
